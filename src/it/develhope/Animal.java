68402f83e82085a8457a424e5d590bdf9927138a
alessandroo94 <121314484+alessandroo94@users.noreply.github.com>
2023-01-17 16:17:21 +0100

    Update Animal.java

--- src/it/develhope/Animal.java
+++ src/it/develhope/Animal.java
@@ -1,2 +1,25 @@
-package it.develhope;public class Animal {
+package it.develhope;
+
+public class Animal {
+    public String animalName;
+
+    public Animal(String animalName){
+        this.animalName = animalName;
+    }
+
+    public void animalSound(){
+        System.out.println("Roarr!");
+    }
+
+    public void animalSound(String intensity){
+        if(intensity.equals("High")){
+            System.out.println("Roarrrrrrrr!");
+        }else if
+            (intensity.equals("Low"))
+         {
+            System.out.println(("Roar"));
+        }else {
+            System.out.println("Cannot reproduce it properly");
+        }
+    }
 }
